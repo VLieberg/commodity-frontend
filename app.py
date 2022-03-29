14a484c7bdb1a82191704f7733628c05b37d7823
Vanessa Lieberg <vlieberg@outlook.com>
2022-03-29 13:46:48 +0100

    atualizando sidebar

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ url = 'https://soybean-price-prediction-ts7i4cg3ha-ew.a.run.app/predict'
 resposta = requests.get(url).json()
 respostalista = list(resposta)
 
-
+# Sidebar:
+st.sidebar.write('<style>div.css-1adrfps{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
 st.sidebar.write("<h1 style='text-align: center; color: black;'>Soybean Futures</h1>", unsafe_allow_html=True)
 st.sidebar.write("<h3 style='text-align: center; color: black;'>Contract Specs</h3>", unsafe_allow_html=True)
 st.sidebar.write("<h3 style='text-align: center; color: gray;'>CME Globex: ZS <br>Contract: 5,000 bushels<br>Price $: U.S. cents per bushel</h3>", unsafe_allow_html=True)
@@ -27,7 +28,8 @@ x = st.sidebar.date_input("Select a date:",datetime.date(2022, 3, 22), max_value
 x = str(x)
 x = x.replace('-','/')
 predict_price = round(resposta[x],2)
-st.sidebar.metric(label=f"Predicted Price at {x}", value=f'${round(resposta[x],2)}', delta=(f'{round((predict_price-1691.00)/1691,2)}%, {round((predict_price-1691.00),2)}$'))
+col1, col2, col3 = st.sidebar.columns([1.3, 5, 0.2])
+col2.metric(label=f"Predicted Price at {x}", value=f'${round(resposta[x],2)}', delta=(f'{round((predict_price-1691.00)/1691,2)}%, {round((predict_price-1691.00),2)}$'))
 imagem_teste = st.sidebar.image("soybean_image.png", width = 350 )
 st.sidebar.write("<h2 style='text-align: center; color: black;'>Model Developed by:</h2>", unsafe_allow_html=True)
 st.sidebar.write("<h4 style='text-align: center; color: gray;'>Alexandre Valadares,<br>Felipe Buongermino,<br>Vanessa Lieberg</h4>", unsafe_allow_html=True)
